d9572a44961b5b51e9eb092001ab4a5e202f4aa3
Sneha Y. Choudhari <snehachoudhari13@gmail.com>
2023-06-16 09:12:10 +0530

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 /** @format */
 
-let myAge = 12;
-let humanDogRatio = 4;
-myDofAge = myAge * humanDogRatio;
-console.log(myDofAge);
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
+}
